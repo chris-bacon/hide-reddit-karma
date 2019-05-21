8b8a2b01e017e3d91dce2587248035904b9134e5
Chris Bacon <chrisbacon2009@hotmail.com>
2019-05-21 22:37:47 +0100

    init

---
+++ hide.js
@@ -0,0 +1,38 @@
+const upvoteButtons = {
+  data: document.querySelectorAll("button[id^=upvote-button]"),
+  f: upvoteButton => {
+    var karmaScore = upvoteButton.nextSibling
+    if (karmaScore) var downvoteButton = upvoteButton.nextSibling.nextSibling
+
+    deleteHtml(upvoteButton)
+    deleteHtml(karmaScore)
+    deleteHtml(downvoteButton)
+  }
+}
+
+const ariaLabelUpvoteButtons = {
+  data: document.querySelectorAll("button[aria-label^=upvote]"),
+  f: ariaUpvoteButton => {
+    var downvoteButton = ariaUpvoteButton.nextSibling
+
+    deleteHtml(ariaUpvoteButton)
+    deleteHtml(downvoteButton)
+  }
+}
+
+const commentPoints = {
+  data: document.querySelectorAll(".Comment > div > div > span"),
+  f: deleteHtml
+}
+
+const targets = [upvoteButtons, ariaLabelUpvoteButtons, commentPoints]
+
+function deleteHtml(target) {
+  if (target && target.parentNode) target.parentNode.removeChild(target)
+}
+
+const run = ts => ts.forEach(target => target.data.forEach(d => target.f(d)))
+
+run(targets)
+
+window.addEventListener('load', () => run(targets));
