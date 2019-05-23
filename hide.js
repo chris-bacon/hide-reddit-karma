410ff9a9255ffd58d21238a9e77c9213fc4a204a
Jacob Clark <jacob.jh.clark@googlemail.com>
2019-05-23 16:06:58 +0100

    Array API

--- hide.js
+++ hide.js
@@ -1,38 +1,35 @@
 const upvoteButtons = {
   data: document.querySelectorAll("button[id^=upvote-button]"),
   f: upvoteButton => {
-    var karmaScore = upvoteButton.nextSibling
+    var karmaScore = upvoteButton.nextSibling;
     
     if (karmaScore) {
-      deleteHtml(upvoteButton)
-      deleteHtml(karmaScore)
-      deleteHtml(downvoteButton)
+      var downvoteButton = upvoteButton.nextSibling.nextSibling
+      deleteHtmlNodes([upvoteButton, karmaScore, downvoteButton]);
   }
 }
 
 const ariaLabelUpvoteButtons = {
   data: document.querySelectorAll("button[aria-label^=upvote]"),
   f: ariaUpvoteButton => {
-    var downvoteButton = ariaUpvoteButton.nextSibling
-
-    deleteHtml(ariaUpvoteButton)
-    deleteHtml(downvoteButton)
+    var downvoteButton = ariaUpvoteButton.nextSibling;
+    deleteHtmlNodes([ariaUpvoteButton, downvoteButton]);
   }
 }
 
 const commentPoints = {
   data: document.querySelectorAll(".Comment > div > div > span"),
-  f: deleteHtml
+  f: deleteHtmlNodes
 }
 
 // ---------------------------------------
 
-const targets = [upvoteButtons, ariaLabelUpvoteButtons, commentPoints]
+const targets = [upvoteButtons, ariaLabelUpvoteButtons, commentPoints];
 
 // ---------------------------------------
 
-function deleteHtml(target) {
-  if (target && target.parentNode) target.parentNode.removeChild(target)
+function deleteHtmlNodes(targets) {
+  targets.forEach(target => if(target.parentNode) target.parentNode.removeChild(target));
 }
 
 // ---------------------------------------
@@ -41,6 +38,6 @@ const run = ts => ts.forEach(target => target.data.forEach(d => target.f))
 
 // ---------------------------------------
 
-run(targets)
+run(targets);
 
 window.addEventListener('load', () => run(targets));
